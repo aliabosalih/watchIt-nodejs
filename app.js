9635450edf7540c4ba097fb6c14e65c73e0c0162
ali <ali.abosalih@allmuze.com>
2018-05-22 18:22:55 +0300

    collections

--- app.js
+++ app.js
@@ -5,29 +5,77 @@ var app = express();
 let mongoose = require('mongoose');
 let db = "mongodb://ds231460.mlab.com:31460/watchit-db"
 let db_options = {
-	  "user": "watchit",
-          "pass" : "!Q@W#E$R%T6y",
-	  "ssl": false
-	};
+    "user": "watchit",
+    "pass": "!Q@W#E$R%T6y",
+    "ssl": false
+};
 var Schema = mongoose.Schema;
 
 
-
-
-
-let  userSchema = new Schema({"name": String});
+let userSchema = new Schema({"name": String,
+    "age": Number,
+    "facebookId": String,
+    "image": String});
 let user = mongoose.model('users', userSchema);
 
 
-let mongodb = mongoose.createConnection(db,db_options);
-let users = mongodb.model('users');
-users.find({"name":"ali"},function(err,up){
-console.log(err,up);
-
+let movieSchema = new Schema({
+    "name": String,
+    "description": String,
+    "runTime": String,
+    "image": String,
+    "language": String,
+    "Genre": String,
+    "Released": String,
+    "imdbRatings": String,
+    "watchitRatings": String,
+    "Writer": String,
+    "Awards": String
 });
+let movies = mongoose.model('movieSchema', movieSchema);
+
+
+let userMovieSchema = new Schema({
+    "movieId": Schema.ObjectId,
+    "userId": Schema.ObjectId,
+    "relation": String,
+    "rate": Number
+});
+let userMovieRelation = mongoose.model('userMovieRelation', userMovieSchema);
+
+
+let commentSchema = new Schema({
+    "movieId": Schema.ObjectId,
+    "userId": Schema.ObjectId,
+    "comment": String,
+    "user": {
+        "name": String,
+        "age": Number,
+        "facebookId": String,
+        "image": String
+    }
+});
+let commentsSchema = mongoose.model('commentSchema', commentSchema);
+
+
+let mongodb = mongoose.createConnection(db, db_options);
+let users = mongodb.model('users');
+
+
+let a = new users();
+a.name = "ali";
+a.age= 12;
+a.facebookId= "12131";
+a.image= ",,,,,,,,,,";
+
+// a.save(function (err, p) {
+//     console.log("............", p)
+// });
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
