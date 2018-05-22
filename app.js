8bed9370c870b637584bc22fb13fe297fc62e66a
ali <ali.abosalih@allmuze.com>
2018-05-22 14:50:25 +0300

    mongo connection

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var app = express();
+
+
+let mongoose = require('mongoose');
+let db = "mongodb://ds231460.mlab.com:31460/watchit-db"
+let db_options = {
+	  "user": "watchit",
+          "pass" : "!Q@W#E$R%T6y",
+	  "ssl": false
+	};
+var Schema = mongoose.Schema;
+
+
+
+
+
+let  userSchema = new Schema({"name": String});
+let user = mongoose.model('users', userSchema);
+
+
+let mongodb = mongoose.createConnection(db,db_options);
+let users = mongodb.model('users');
+users.find({"name":"ali"},function(err,up){
+console.log(err,up);
+
+});
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
