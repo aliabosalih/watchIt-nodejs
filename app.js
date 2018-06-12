ebb3943f82115fc331b94fe7c7ec63b5dbc99261
ali <ali.abosalih@allmuze.com>
2018-06-12 21:43:53 +0300

    fcm added

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-
-const express = require('express') , 
+'use strict';
+const express = require('express') ,
     app = express(),
     compression = require('compression'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
-    usersRoute = require('./routes/user'); 
-    movieRoute = require('./routes/movie') ;
-    reviewsRoute = require('./routes/review')
+    usersRoute = require('./routes/user'),
+    movieRoute = require('./routes/movie'),
+    reviewsRoute = require('./routes/review'),
+    fcmRoute = require('./routes/fcm');
 
 app.use(morgan('combined'));
 app.use(compression());
 app.use(bodyParser.json({defer: true, limit: '50mb'}));
 
+
+
+
 app.use('/users' , usersRoute);
 app.use('/movies' , movieRoute);
+app.use('/fcm' , fcmRoute);
 app.use('/reviews' , reviewsRoute);
 
 let port = process.env.PORT;
